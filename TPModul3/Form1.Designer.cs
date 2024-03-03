0192df49f462ea40c594b371ada4f02b0d5d83a8
shad kinx <rivapermana763@gmail,com>
2024-03-03 23:41:25 +0700

    First Commit

---
+++ TPModul3/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TPModul3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(40, 66);
+            button1.Name = "button1";
+            button1.Size = new Size(417, 49);
+            button1.TabIndex = 0;
+            button1.Text = "INPUT";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(528, 66);
+            button2.Name = "button2";
+            button2.Size = new Size(160, 49);
+            button2.TabIndex = 1;
+            button2.Text = "BUTTON";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(40, 188);
+            button3.Name = "button3";
+            button3.Size = new Size(648, 49);
+            button3.TabIndex = 2;
+            button3.Text = "OUTPUT";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(765, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
